1679f4614f22495c7935c0f3ce4d3c0d8f85e8d0
Ahmed BOURAZZA <e2405908@ensimlan.univ-lemans.fr>
2024-11-18 15:50:07 +0100

    qst etape 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,25 +5,47 @@ import model.Client;
 import model.Compte;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         ArrayList<Compte> comptes = new ArrayList<Compte>();
-        comptes.add(new Compte(1,1562,new Client("BOURAZZA","Ahmed")));
+      /*  comptes.add(new Compte(1,1562,new Client("BOURAZZA","Ahmed")));
         comptes.add(new Compte(2,150,new Client("MAKHLOUL","Ilyas")));
         comptes.add(new Compte(3,10000,new Client("ammidouah","Adnan")));
 
+*/
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez entrer le nombre de compte à ajouter ");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Veuillez entrer le numero de compte "+(i+1));
+            int num = scanner.nextInt();
+            System.out.println("Veuillez entrer le solde de compte ");
+            float solde = scanner.nextFloat();
+            System.out.println("Veuillez entrer le nom du client ");
+            String nom = scanner.next();
+            System.out.println("Veuillez entrer le prenom du client ");
+            String prenom = scanner.next();
+            comptes.add(new Compte(num,solde,new Client(nom,prenom)));
+
+        }
+        System.out.println("-----------------------------------------------");
+
+        System.out.println("Voici la liste des comptes");
 
         for (Compte compte : comptes) {
             System.out.println(compte);
         }
-        // Tri des comptes par solde croissant
+
+        System.out.println("-----------------------------------------------");
+        System.out.println("Voici la liste des comptes triés selon le solde");
+
         BanqueService banqueService = new BanqueService();
         ArrayList<Compte> comptesTries = banqueService.sortBySolde(comptes);
 
-        // Affichage des comptes triés
         System.out.println("\nListe des comptes après tri :");
         for (Compte compte : comptesTries) {
             System.out.println(compte);
