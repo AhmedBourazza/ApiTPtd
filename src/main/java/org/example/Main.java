5945ec514933466a52c22b625ddea22be3c0a7ae
Ahmed BOURAZZA <e2405908@ensimlan.univ-lemans.fr>
2024-11-18 16:01:34 +0100

    qst  etape 4 : e

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import Service.BanqueService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import model.Client;
 import model.Compte;
 
@@ -12,11 +14,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Compte> comptes = new ArrayList<Compte>();
-      /*  comptes.add(new Compte(1,1562,new Client("BOURAZZA","Ahmed")));
+       comptes.add(new Compte(1,1562,new Client("BOURAZZA","Ahmed")));
         comptes.add(new Compte(2,150,new Client("MAKHLOUL","Ilyas")));
         comptes.add(new Compte(3,10000,new Client("ammidouah","Adnan")));
 
-*/
+/*
         Scanner scanner = new Scanner(System.in);
         System.out.println("Veuillez entrer le nombre de compte à ajouter ");
         int n = scanner.nextInt();
@@ -31,7 +33,7 @@ public class Main {
             String prenom = scanner.next();
             comptes.add(new Compte(num,solde,new Client(nom,prenom)));
 
-        }
+        }*/
         System.out.println("-----------------------------------------------");
 
         System.out.println("Voici la liste des comptes");
@@ -50,6 +52,14 @@ public class Main {
         for (Compte compte : comptesTries) {
             System.out.println(compte);
         }
+        System.out.println("-----------------------------------------------");
+
+        // Affichage des comptes triés en JSON
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String comptesJson = gson.toJson(comptesTries);
+
+        System.out.println("\nListe des comptes triés (format JSON) :");
+        System.out.println(comptesJson);
 
     }
 }
